c16f62d555ba9822f4ce847bcfd8816a5f5734ac
GeekyHacks <geekyhacks22@gmail.com>
2023-07-10 21:22:20 +0300

    Setup all required modules& connect them correctly

--- JS/modules/dateTime.js
+++ JS/modules/dateTime.js
@@ -1,2 +1,30 @@
-const now = DateTime.now();
-dt = DateTime.fromObject({ day: 22, hour: 12 }, { zone: 'America/Los_Angeles', numberingSystem: 'beng' });
+const dateLine = document.querySelector('.dayDate');
+const navBar = document.querySelector('nav');
+import { DateTime } from './luxon.js';
+
+export let displayDate = () => {
+  const dt = DateTime.now();
+  let f = { month: 'long', day: 'numeric' };
+  dt.setLocale('en-US').toLocaleString(f);
+  dateLine.innerHTML = `${dt}`;
+  navBar.appendChild(dateLine);
+};
+
+// var dt = DateTime.now();
+// var f = {month: 'long', day: 'numeric'};
+// dt.setLocale('fr').toLocaleString(f)      //=> '14 septembre'
+// dt.setLocale('en-GB').toLocaleString(f)   //=> '14 September'
+// dt.setLocale('en-US').toLocaleString(f)
+
+// export let displayDate = () => {
+//   const stringDate = new Date();
+//   const date = stringDate.toLocaleDateString('en-us', {
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric',
+//   });
+//   const time = stringDate.toLocaleTimeString();
+//   dateLine.innerHTML = `${date} ${time}`;
+//   navBar.appendChild(dateLine);
+// };
+
---
+++ JS/modules/navBar.js
@@ -0,0 +1,78 @@
+import { showPopWin, main } from '../index.js';
+import { container, displayBooks } from '../modules/displayBooks.js';
+import { Book } from '../modules/books.js';
+
+const contact = () => {
+  container.innerHTML = `<h2>Contact Information</h2>
+    <h3>Reach out to us whenever you have any question or wanna say 'Hello!'</h3>
+    <ul>
+      <li>Author:geekyhacks22@gmail.com</li>
+      <li>Phone:0032112321</li>
+      <li>Adress:Zaid Street, Sana'a, Yemen</li>
+    </ul>`;
+  main.appendChild(container);
+};
+
+// List page
+const listpage = () => displayBooks(container);
+// addnew page
+const addBook = () => {
+  container.innerHTML = `
+  <h2 class="hline">Add New Book</h2>
+   
+    <form action="" class="bookForm">
+      <input required id="title" type="text" placeholder="Title" />
+     <input required id="author" type="text" placeholder="Author" />
+     <button class="button" id="btn" type="submit">Add</button>
+    </form>`;
+  main.appendChild(container);
+  const addBtn = document.querySelector('#btn');
+  const titleInput = document.querySelector('#title');
+  const authorInput = document.querySelector('#author');
+  // Declare an data object to store userinput
+  let formData = {
+    Title: '',
+    Author: '',
+  };
+
+  // Declare the userinput as a data and match it with dataobject
+  const formUserInput = (data) => {
+    titleInput.value = data.Title;
+    authorInput.value = data.Author;
+  };
+
+  titleInput.addEventListener('input', () => {
+    formData.Title = titleInput.value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+
+  authorInput.addEventListener('input', () => {
+    formData.Author = authorInput.value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+
+  // Store all user input one by one
+  if (localStorage.getItem('formData')) {
+    formData = JSON.parse(localStorage.getItem('formData'));
+    formUserInput(formData);
+  }
+
+  addBtn.addEventListener('click', (event) => {
+    const title = titleInput.value;
+    const author = authorInput.value;
+    if (title === '' || author === '') {
+      return null;
+    }
+
+    Book.addBook(title, author);
+    localStorage.removeItem('formData');
+    titleInput.value = '';
+    authorInput.value = '';
+    showPopWin();
+    addBook();
+    return event.preventDefault();
+  });
+};
+
+export { listpage, contact, addBook };
+
